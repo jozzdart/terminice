ac4c979b1bd979ad245da1ef7a68e38d017cd970
jozz <yosefd99@gmail.com>
2025-12-22 20:17:17 +0200

    Exported terminal system types

--- terminice/lib/src/core/core_exports.dart
+++ terminice/lib/src/core/core_exports.dart
@@ -1,2 +1,13 @@
 export 'package:terminice_core/terminice_core.dart'
-    show PromptTheme, PromptStyle, BadgeTone;
+    show
+        PromptTheme,
+        PromptStyle,
+        BadgeTone,
+        // Terminal I/O abstraction
+        Terminal,
+        TerminalInput,
+        TerminalOutput,
+        DartTerminal,
+        DartTerminalInput,
+        DartTerminalOutput,
+        TerminalContext;
